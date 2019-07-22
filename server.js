7d87b8f6e6a04841b7928818150a94811f0d1637
edfranco <edfranco415@gmail.com>
2019-07-22 10:34:47 -0700

    set up public and views files and linked them together to serve a viewable static page

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, => console.log(`Server is live`));
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`);
+})
+
+app.listen(3000, () => console.log(`Server is live`));
