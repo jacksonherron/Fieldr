742ca07a8408d83eb02313002cd3c73c7dc6a855
edfranco <edfranco415@gmail.com>
2019-07-22 16:02:47 -0700

    added hash to encrypt passwords

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,16 +1,7 @@
 const db = require('../models');
+const bcrypt = require('bcryptjs');
 
 const createUser = (req, res) => {
-    db.User.create(req.body, (error, createdUser) => {
-        if (error) return console.log(error);
-        res.render(`/profile/show.ejs`)
-    })
-}
-
-const newSession = (req, res) => {
-    res.render('users/login');
-}
-const createSession = (req, res) => {
     const errors = [];
     if (!req.body.email) {
         errors.push({ field: 'email', message: 'Did not enter a valid email' });
@@ -23,6 +14,32 @@ const createSession = (req, res) => {
     if (errors.length) {
         return res.render('users/login', { errors })
     }
+
+    // Generate Hashed Password
+    bcrypt.genSalt(10, (error, salt) => {
+        if (error) return res.render(`/users/signup`, { errors: [{ message: 'Something went wrong. Try Again' }] })
+
+        bcrypt.hash(req.body.password, salt, (error, hash) => {
+            if (error) return res.render('users/signup', { errors: [{ message: 'Something went wrong. Try again' }] });
+            req.body.password = hash;
+
+            db.User.create(req.body, (error, createdUser) => {
+                if (error) return console.log(error);
+                res.json({
+                    data: createdUser
+                })
+            })
+        })
+
+    });
+
+}
+
+const newSession = (req, res) => {
+    res.render('users/login');
+}
+const createSession = (req, res) => {
+
 }
 
 const newUser = (req, res) => {
