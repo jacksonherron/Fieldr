0511c27c2079496b70bba46b1e392b625d09b74a
edfranco <edfranco415@gmail.com>
2019-07-23 15:03:43 -0700

    created template for users wall

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,30 @@
 console.log('Meet me at the park! ⚾️');
+
+const cardTemplate = `
+<div class="wall">       
+    <div class="card">
+            <div class="sport">
+                Baseball
+            </div>
+
+            <div class="descr">
+                <h1>User</h1>
+                Join us for this sport at the park
+            </div>
+        </div>
+
+        <div class="comment-field">
+            <form>
+                <div class="inputs">
+                    <input type="text"/>
+                     <input type="submit">
+                </div>
+            </form?
+        </div>
+</div>
+`
+
+const userWall = document.getElementById('wall');
+
+userWall.insertAdjacentHTML('afterbegin', cardTemplate);
+
