8e414653045eb33752f18ad25cd165e8d697cdd1
jacksonherron <jacksonherron@gmail.com>
2019-07-23 12:03:02 -0700

    Updates home route

--- controllers/postsCtrl.js
+++ controllers/postsCtrl.js
@@ -1,1 +1,9 @@
 const db = require('../models');
+
+const showHomePage = (req, res) => {
+    res.render('profile/show.ejs', { currentUser: req.session.currentUser });
+};
+
+module.exports = {
+    showHomePage,
+}
